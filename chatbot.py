762d3a3a4767644e193f2c6c12f2daa8d1a08ef8
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2024-06-07 07:59:21 -0500

    Initial commit

--- chatbot.py
+++ chatbot.py
@@ -2,13 +2,32 @@ from openai import OpenAI
 
 client = OpenAI()
 
+# accepts a preferred model (in this case "gpt-3.5-turbo") and a list of messages
+# makes chat completions API call
+# returns the response message content
+
+def get_api_chat_response_message(model, messages):
+    # make the API call
+    api_response = client.chat.completions.create(
+    model = model,
+    messages = messages
+    )
+    
+    # extract the response text
+    response_content = api_response.choices[0].message.content
+
+    # return the response text
+    return response_content
+
+
+# prompt the user to ask a question
 user_input = input("\nAsk something...\n\n")
 
 model = "gpt-3.5-turbo"
 
 messages = [
     {
-        "role": "system",
+        "role": "system",            
         "content": "You are an assistant that always answers in the form of a poem."
     },
     {
@@ -17,11 +36,6 @@ messages = [
     }
 ]
 
-response = client.chat.completions.create(
-    model = model,
-    messages = messages
-)
-
-response_for_user = response.choices[0].message.content
+response_for_user = get_api_chat_response_message(model, messages)
 
 print("\n" + response_for_user + "\n")
